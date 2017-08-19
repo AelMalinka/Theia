52d2ea435dec0dbf70fa3e661680064c65719b52
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-08-19 17:23:25 -0400

    add actual test like stuff to Windows Test

--- win/WindowsTest/main.cc
+++ win/WindowsTest/main.cc
@@ -7,14 +7,39 @@
 #include <vector>
 
 #include <Entropy/Theia/Application.hh>
+#include <Entropy/Theia/Object.hh>
+#include <Entropy/Theia/GL/Array.hh>
+#include <Entropy/Theia/GL/Bind.hh>
+#include <Entropy/Theia/Utility/SharedData.hh>
 
 using namespace std;
 using namespace Entropy::Theia;
+using namespace Entropy::Theia::GL;
 
 const size_t MAX_BUFFSIZE = 4096;
 
-class MyObject
-{};
+namespace detail
+{
+	struct shared_data
+	{
+		shared_data();
+		Program program;
+		Array array;
+	};
+}
+
+class MyObject :
+	private SharedData<detail::shared_data>,
+	public Object
+{
+	public:
+		MyObject();
+		void Draw();
+	private:
+		void UpdateModel();
+		void UpdateCamera(const Camera &);
+		void UpdateScreen(const Screen &);
+};
 
 class Application :
 	public Entropy::Theia::Application
@@ -34,27 +59,115 @@ int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	{
 		::Application app(moduleName);
 		app();
+
+		return EXIT_SUCCESS;
 	}
 	catch (exception &e)
 	{
 		MessageBox(nullptr, e.what(), "Error!", MB_ICONEXCLAMATION | MB_OK);
 		return EXIT_FAILURE;
 	}
-
-	return EXIT_SUCCESS;
 }
-
-::Application::Application(const string &name)
-	: Entropy::Theia::Application(name)
-{}
 
 void ::Application::onEvent(const Event &ev)
 {
 	if (ev.Id() == Events::Key::Id) {
 		const Events::Key &k = dynamic_cast<const Events::Key &>(ev);
 
-		if (k.Action() == GLFW_PRESS && k.Code() == GLFW_KEY_ESCAPE) {
-			Windows()->Close();
+		if (k.Action() == GLFW_PRESS) {
+			switch (k.Code()) {
+				case GLFW_KEY_ESCAPE:
+					Windows()->Close();
+				break;
+				case GLFW_KEY_F:
+					Windows()->Fullscreen();
+				break;
+			}
 		}
 	}
+}
+
+void MyObject::Draw()
+{
+	Bind p(shared()->program), a(shared()->array);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+detail::shared_data::shared_data()
+	: program(), array()
+{
+	string vert_code =
+		"#version 130\n"
+
+		"uniform mat4 model;"
+		"uniform mat4 view;"
+		"uniform mat4 projection;"
+
+		"in vec2 in_position;"
+
+		"void main() {"
+		"gl_Position = projection * view * model * vec4(in_position, 0.0, 1.0);"
+		"}"
+		;
+
+	string frag_code =
+		"#version 130\n"
+
+		"out vec4 out_color;"
+
+		"void main() {"
+		"out_color = vec4(1.0, 1.0, 1.0, 1.0);"
+		"}"
+		;
+
+	vector<float> vertices = {
+		-1.0, 1.0f,
+		1.0f, 1.0f,
+		-1.0f, -1.0f,
+		1.0f, -1.0f,
+	};
+
+	Shader vert(Shader::Vertex, vert_code);
+	Shader frag(Shader::Fragment, frag_code);
+
+	program.Attach(vert);
+	program.Attach(frag);
+	program.Link();
+
+	Buffer vbo(Buffer::Vertex);
+	vbo.Data(vertices, Buffer::Static);
+	array.Bind(program, vbo, "in_position"s, 2, GL_FLOAT);
+}
+
+::Application::Application(const string &name)
+	: Entropy::Theia::Application(name)
+{
+	Windows()->setFoV(90.0);
+	Windows()->setNearClipping(0.0);
+	Windows()->setFarClipping(10.0);
+
+	Windows()->Scenes()->getCamera().setPosition(Vertex(0.0, 0.0, 10.0));
+	Windows()->Scenes()->getCamera().setLookAt(Vertex(0.0, 0.0, 0.0));
+	Windows()->Scenes()->getCamera().setUp(Vertex(0.0, 1.0, 0.0));
+
+	Windows()->Scenes()->emplaceDrawable<MyObject>();
+}
+
+MyObject::MyObject()
+	: SharedData<detail::shared_data>(), Object()
+{}
+
+void MyObject::UpdateModel()
+{
+	shared()->program.SetUniform("model"s, Model());
+}
+
+void MyObject::UpdateCamera(const Camera &c)
+{
+	shared()->program.SetUniform("view"s, c.View());
+}
+
+void MyObject::UpdateScreen(const Screen &s)
+{
+	shared()->program.SetUniform("projection"s, s.Perspective());
 }
