385ca09bf42131793e08ed187ce4133a443d687d
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-10-08 17:35:59 -0400

    add skeleton tests to windows build

---
+++ test/basic/application.cc
@@ -0,0 +1,15 @@
+/*	Copyright 2017 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#include <gtest/gtest.h>
+#include "Application.hh"
+
+using namespace std;
+using namespace Entropy::Theia;
+
+namespace {
+	TEST(ApplicationTests, Create) {
+		Application app;
+	}
+}
